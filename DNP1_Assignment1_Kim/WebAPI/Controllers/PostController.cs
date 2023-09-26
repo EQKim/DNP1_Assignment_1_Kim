57ffe867c156608545046dca15fa1a030508e8e4
EQKim <114055097+EQKim@users.noreply.github.com>
2023-09-26 11:55:45 +0200

    DoneV1

--- DNP1_Assignment1_Kim/WebAPI/Controllers/PostController.cs
+++ DNP1_Assignment1_Kim/WebAPI/Controllers/PostController.cs
@@ -9,20 +9,20 @@ namespace WebAPI.Controllers;
 public class PostController : ControllerBase
 {
     private static readonly List<Post> Posts = new List<Post>();
-    
-    
-    //Getter
+
+
+    //Get All Posts
     [HttpGet]
     public ActionResult<IEnumerable<Post>> GetPosts()
-    {   
+    {
         //Fields 
         var foldername = "JSON_Storage";
         var posts = new List<Post>();
-        
+
         //Guard
         if (Directory.Exists(foldername))
         {
-            var files = Directory.GetFiles(foldername, "*.json");
+            var files = Directory.GetFiles(foldername, "Post_*.json");
             foreach (var file in files)
             {
                 try
@@ -42,9 +42,41 @@ public class PostController : ControllerBase
         return posts;
     }
 
+    
+    //Get Specified Post
+    [HttpGet("ByTitle")]
+    public ActionResult<IEnumerable<Post>> GetPostFromTitle(String title)
+    {
+        var foldername = "JSON_Storage";
+        var posts = new List<Post>();
 
-    
-    
+        if (Directory.Exists(foldername))
+        {
+            var files = Directory.GetFiles(foldername, "Post_*.json");
+            foreach (var file in files)
+            {
+                try
+                {
+                    var json = System.IO.File.ReadAllText(file);
+                    var post = JsonSerializer.Deserialize<Post>(json);
+
+                    if (post.title.Equals(title))
+                    {
+                        posts.Add(post);
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+        return posts;
+    }
+
+
     //Create
     [HttpPost]
     public async Task<ActionResult<Post>> CreatePost([FromBody] Post newPost)
@@ -52,25 +84,25 @@ public class PostController : ControllerBase
         newPost.ID = Posts.Count + 1;
         Posts.Add(newPost);
         var foldername = "JSON_Storage";
-        
-        
+
+
         //Guards
         if (!Directory.Exists("JSON_Storage"))
         {
             Directory.CreateDirectory(foldername);
         }
-        
+
         if (string.IsNullOrEmpty(newPost.description))
         {
             throw new Exception("Your description cant be empty");
         }
-        
+
         if (string.IsNullOrEmpty(newPost.title))
         {
             throw new Exception("Your title cant be empty");
         }
-        
-        
+
+
         //JSON
         var json = JsonSerializer.Serialize(newPost);
         var fileName = $"{foldername}/Post_{newPost.ID}.json";
