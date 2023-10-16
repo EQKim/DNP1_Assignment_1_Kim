ffc8b68e7c5d761bfe90938b3f72558e97ac8c3e
EQKim <114055097+EQKim@users.noreply.github.com>
2023-10-16 19:42:40 +0200

    Frontend and API working

--- DNP1_Assignment1_Kim/BlazorApp1/Program.cs
+++ DNP1_Assignment1_Kim/BlazorApp1/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,26 @@ builder.Services.AddServerSideBlazor();
 //HTTP Client
 var services = builder.Services;
 services.AddScoped<HttpClient>();
+
+//-----------------------------BLACK MAGIC----------------------------
+// Bypass SSL validation (For Development ONLY!)
+services.AddTransient(sp => new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+});
+
+// Configure HttpClient with the handler
+services.AddHttpClient("NoSSL", c =>
+    {
+        c.BaseAddress = new Uri("https://localhost:7115"); // adjust if needed
+    })
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    });
+
+//-----------------------------BLACK MAGIC----------------------------
+
 
 
 //Build
